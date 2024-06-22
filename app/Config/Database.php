a1158f0c901d055d570d3ac00cf111a369e181e3
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2024-06-22 18:22:20 +0800

    push

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'cio_db',
+        'username'     => 'ciocalapan',
+        'password'     => 'CalapanCity_2324',
+        'database'     => 'u262559041_ciocalapan',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
