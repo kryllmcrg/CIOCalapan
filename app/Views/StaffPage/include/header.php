b930a0052a92e2dad742a97dd20713bbae453344
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2024-08-25 12:16:01 +0800

    Update header.php

--- app/Views/StaffPage/include/header.php
+++ app/Views/StaffPage/include/header.php
@@ -2,19 +2,19 @@
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-menu"></span>
           </button>
-          <div class="search-field d-none d-md-block">
+          <!-- <div class="search-field d-none d-md-block">
             <form class="d-flex align-items-center h-100" action="#">
               <div class="input-group">
                 <div class="input-group-prepend bg-transparent">
                   <i class="input-group-text border-0 mdi mdi-magnify"></i>
                 </div>
-                <!-- <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects"> -->
+                <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects">
               </div>
             </form>
-          </div>
+          </div> -->
           <ul class="navbar-nav navbar-nav-right">
            
-            <li class="nav-item d-none d-lg-block full-screen-link">
+            <!-- <li class="nav-item d-none d-lg-block full-screen-link">
               <a class="nav-link">
                 <i class="mdi mdi-fullscreen" id="fullscreen-button"></i>
               </a>
@@ -26,10 +26,9 @@
               </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
                 
-
               </div>
-            </li>
-            <li class="nav-item dropdown">
+            </li> -->
+            <!-- <li class="nav-item dropdown">
               <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
                 <i class="mdi mdi-bell-outline"></i>
                 <span class="count-symbol bg-danger"></span>
@@ -42,7 +41,7 @@
                 </a>
                 <div class="dropdown-divider"></div>
               </div>
-            </li>
+            </li> -->
                 <li class="nav-item nav-profile dropdown">
                   <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
                     <div class="nav-profile-img">
