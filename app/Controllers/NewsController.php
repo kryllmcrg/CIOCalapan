28ab9357b7a5747f3a36ea080e31d525ae6376b2
Karylle <macaraigkarylle48@gmail.com>
2024-10-08 19:29:58 +0800

    Update NewsController.php

--- app/Controllers/NewsController.php
+++ app/Controllers/NewsController.php
@@ -524,8 +524,14 @@ class NewsController extends BaseController
         // Render PDF
         $dompdf->render();
 
-        // Output PDF
-        $dompdf->stream($filename, ["Attachment" => 0]);
+        // Output the generated PDF to Browser (inline view, without download prompt)
+        header('Content-Type: application/pdf');
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Pragma: no-cache');
+        header('Content-Disposition: inline; filename="Monthly_News_Report.pdf"');
+
+        // Stream the PDF (with Attachment => false for inline viewing in the browser)
+        $dompdf->stream("Monthly_News_Report.pdf", array("Attachment" => false));
     }
     public function articles()
     {
