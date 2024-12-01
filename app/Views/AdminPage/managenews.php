d92c7b41398e5bc611f95078e2df5ba9222dc311
Karylle <macaraigkarylle48@gmail.com>
2024-12-01 11:41:56 +0800

    Update managenews.php

--- app/Views/AdminPage/managenews.php
+++ app/Views/AdminPage/managenews.php
@@ -141,7 +141,7 @@
                                     <div id="reportPreviewContainer"></div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                     <button type="button" class="btn btn-primary" onclick="generateReport()">Generate Report</button>
                                 </div>
                             </div>
