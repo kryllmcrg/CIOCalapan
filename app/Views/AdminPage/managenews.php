2dec19cc1010ac7ca05d6f265a339be66ef76cc8
Karylle <macaraigkarylle48@gmail.com>
2024-12-01 11:39:45 +0800

    Update managenews.php

--- app/Views/AdminPage/managenews.php
+++ app/Views/AdminPage/managenews.php
@@ -487,6 +487,7 @@ function generateReport() {
     <script src="<?= base_url('assets2/vendors/js/vendor.bundle.base.js')?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
     <script src="<?= base_url('assets2/vendors/chart.js/Chart.min.js')?>"></script>
